262bc50141c4600c00d3383c6ebc026a744c8769
Nilanjan Mukherjee14 <nilamu14@in.ibm.com>
2022-06-23 16:31:22 +0530

    Simple Flask App

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -14,6 +14,6 @@ LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
 
 logging.basicConfig(filename=LOG_FILE_PATH,
 filemode="w",
-format=['%(asctime)s] %(name)s - %(levelname)s - %(message)s',
-level=logging.INFO
+format=['%(asctime)s] %(name)s - %(levelname)s - %(message)s'
+##level=logging.INFO
 )
